246175f43239ccd48110b39b39251a46e181f769
Stephen Hendricks <stephen.hendricks87@gmail.com>
2021-05-12 10:19:31 -0700

    add support for more detailed nicknames on promotion to include unit designation, rank, and gamer tag

--- commands/promote.js
+++ commands/promote.js
@@ -52,16 +52,39 @@ module.exports = {
         }
 
         // give a new nickname
+        // example nickname: 2/1 SGT. PERSONS
         if (member.nickname) {
-            const nickParts = member.nickname.split('. ');
+            const nickParts = member.nickname.split(' ');
 
-            if (nickParts.length < 2) {
-                return message.reply(`${member.nickname} is not in a valid format`);
+            let unitPart = '';
+            let namePart = '';
+
+            for (let i = 0; i < nickParts.length; i++) {
+                // search for the unit part of their nickname, eg 2/1 or [HHC], etc
+                if (nickParts[i].match(/^(\d\/\d|\[\w+])$/) && !unitPart) {
+                    unitPart = nickParts[i] + ' ';
+                }
+
+                // regardless if we found a unit part or not, we should continue on from this iteration
+                if (i === 0) {
+                    continue;
+                }
+
+                // assuming the next iterator is iteration i=1 (second index)
+
+                // search for the rank part of their nickname (if it exists) so it can be excluded when updating their nickname
+                // eg SGT. or PVT. etc
+                if (nickParts[i].match(/^(\w|\/){2,3}\.$/)) {
+                    namePart = nickParts.slice(i + 1).join(' ');
+                } else {
+                    // if we dont find a match to the
+                    namePart = nickParts.slice(i).join(' ');
+                }
+
+                break;
             }
 
-            nickParts.shift();
-
-            await member.setNickname(`${nextRank.name}. ${nickParts.join(' ')}`);
+            await member.setNickname(`${unitPart + nextRank.name}. ${namePart}`);
         } else {
             await member.setNickname(`${nextRank.name}. ${member.user.username}`);
         }
