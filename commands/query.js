486d091172cf93531536588712466f79b3606a6b
Stephen Hendricks <stephen.hendricks87@gmail.com>
2021-07-26 19:33:11 -0700

    member query prototype

--- commands/query.js
+++ commands/query.js
@@ -1,5 +1,8 @@
 const {MessageEmbed} = require('discord.js');
 
+const monthNames = ['JAN', 'FED', 'MAR', 'APR', 'MAY', 'JUN',
+    'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
+
 module.exports = {
     commands           : ['query'],
     expectedArgs       : '<query_of> <search>',
@@ -7,38 +10,50 @@ module.exports = {
     requiredPermissions: ['ADMINISTRATOR'],
     callback           : async (message, args) => {
         // ~query rank RCT
-            
+
         if (args[0] === 'rank') {
             const rankRole = message.mentions.roles.first();
-                
+
             if (rankRole) {
-                // get members of requested rank and stub out data for message
-                const membersOfRank = message.guild.members.cache.filter((member) => member.roles.cache.has(rankRole.id))
-                    .mapValues((member) => {
-                        return {...member, dateOfRank: ''};
-                    });
+                const membersWithRank = message.guild.members.cache.filter((member) => member.roles.cache.has(rankRole.id));
 
-                // pull the audit log to try to get the dates (this is the only way to get this sort of information, and is limited by 90 days history, among other things)
-                const auditLog = await message.guild.fetchAuditLogs({
+                const auditLogs = (await message.guild.fetchAuditLogs({
                     type: 'MEMBER_ROLE_UPDATE'
-                });
+                })).entries.filter((logEntry) => logEntry.changes.length && logEntry.changes[0].key === '$add' && logEntry.targetType === 'USER');
 
-                auditLog.entries.each((entry) => {
-                    const member = membersOfRank.get(entry.id);
+                // iterate over the audit log, trying to match audit entries with our member collection
+                auditLogs.each((logEntry) => {
+                    const member = membersWithRank.get(logEntry.target.id);
 
-                    if (member) {
-                        member.dateOfRank = entry.createdAt;
+                    // if the member in the audit entry matches an entry in our membersWithRank
+                    // collection, AND the role added from the audit entry matches the rankRole
+                    if (member && logEntry.changes[0].new[0].id === rankRole.id) {
+                        member.dateOfRank = logEntry.createdAt;
                     }
                 });
-                    
+
                 const embed = new MessageEmbed()
                     .setColor('#cd1c1c')
                     .setAuthor('Uncle Sam', 'https://thefighting36th.com/img/favicon-16x16.png', 'https://thefighting36th.com')
-                    .setDescription(`Members of rank: ${rankRole.name}`)
+                    .setDescription(`Query by rank: ${rankRole.name}`)
                     .setThumbnail('https://thefighting36th.com/img/favicon-32x32.png')
                     .addFields(
-                        { name: 'Members', value: [...membersOfRank.values()].join('\n'), inline: true },
-                        {name: 'Date of Rank', value: [...membersOfRank.values()].map((member) => member.dateOfRank).join('\n'), inline: true}
+                        {
+                            name  : 'Members',
+                            value : [...membersWithRank.values()].join('\n'),
+                            inline: true
+                        },
+                        {
+                            name : 'Date of Rank',
+                            value: [...membersWithRank.values()].map((member) => {
+                                if (member.dateOfRank) {
+                                    return member.dateOfRank.getDate() + monthNames[member.dateOfRank.getMonth()] + member.dateOfRank.getFullYear();
+                                }
+
+                                return '';
+                            }).join('\n'),
+                            inline: true
+                        }
                     )
                     .setTimestamp()
                     .setFooter('Brought to you by Uncle Sam', 'https://thefighting36th.com/img/favicon-16x16.png');
