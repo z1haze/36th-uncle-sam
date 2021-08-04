ee4677304ecde11f39fbe976dc0f7b45512af7dd
Stephen Hendricks <stephen.hendricks87@gmail.com>
2021-08-03 17:06:10 -0700

    process command permissions from env vars

--- util/permission.js
+++ util/permission.js
@@ -1,45 +1,44 @@
 const setCommandsPermissions = async (commandManager) => {
-    commandManager.cache.each((command) => {
+    const ncoCorpPermission = {
+        id        : process.env.NCO_CORP_ROLE_ID,
+        type      : 'ROLE',
+        permission: true
+    };
+
+    const officerCorePermission = {
+        id        : process.env.OFFICER_CORP_ROLE_ID,
+        type      : 'ROLE',
+        permission: true
+    };
+
+    commandManager.cache.each(async (command) => {
         switch (command.name) {
-            case 'clear': {
+            case 'clear':
+            case 'nickname':
+            case 'transfer':
+                await command.permissions.set({permissions: [ncoCorpPermission, officerCorePermission]});
                 break;
-            }
 
-            case 'dm': {
+            case 'dm':
+            case 'promote':
+            case 'reaction':
+            case 'query':
+                await command.permissions.set({permissions: [officerCorePermission]});
                 break;
-            }
 
-            case 'dump': {
-                break;
-            }
-
-            case 'echo': {
-                break;
-            }
-
-            case 'edit': {
-                break;
-            }
-
-            case 'nickname': {
-                break;
-            }
-
-            case 'promote': {
-                break;
-            }
-
-            case 'query': {
-                break;
-            }
-
-            case 'reaction': {
-                break;
-            }
-
-            case 'transfer': {
-                break;
-            }
+            case 'dump':
+            case 'echo':
+            case 'edit':
+                await command.permissions.set({
+                    permissions: process.env.TOP_TIER_ROLE_IDS.split(',')
+                        .map((roleId) => {
+                            return {
+                                id        : roleId,
+                                type      : 'ROLE',
+                                permission: true
+                            };
+                        })
+                });
         }
     });
 };
