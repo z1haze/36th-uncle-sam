cf02ef70166b417474650335c4e9a9d53962f90a
Stephen Hendricks <stephen.hendricks87@gmail.com>
2021-08-04 17:47:27 -0700

    fix 3 for updatenicks

--- commands/updatenicks.js
+++ commands/updatenicks.js
@@ -3,22 +3,25 @@ const {setNickName} = require('../util/user');
 module.exports = (interaction) => {
     const onlineOnly = interaction.options.getBoolean('onlineonly');
 
-    let members = null;
-
-    if (onlineOnly) {
-        members = interaction.guild.members.cache.filter((member) => member.presence.status === 'online');
-    } else {
-        members = interaction.guild.members.cache;
-    }
-
-    const promises = [];
-
     interaction.defer({ephemeral: true})
-        .then(() => {
+        .then(async () => {
+            await interaction.guild.members.fetch();
+
+            let members = null;
+
+            if (onlineOnly) {
+                members = interaction.guild.members.cache.filter((member) => member.presence.status === 'online');
+            } else {
+                members = interaction.guild.members.cache;
+            }
+
+            const promises = [];
+            
             members.each((member) => promises.push(setNickName(member)));
 
             Promise.all(promises)
                 .then(() => interaction.editReply(`Updated nicknames for ${members.size()}`));
         })
         .catch((e) => interaction.editReply(e.message));
+
 };
