3174acc0c44f95190597bb5de6d85f96a0961838
Stephen Hendricks <stephen.hendricks87@gmail.com>
2021-09-11 11:50:14 -0700

    update the echo command to work through a DM

--- commands/echo.js
+++ commands/echo.js
@@ -1,7 +1,63 @@
 module.exports = async (interaction) => {
-    await interaction.channel.send(interaction.options.get('message').value);
-    await interaction.reply({
-        content  : 'Message echoed.',
-        ephemeral: true
+    await interaction.deferReply({ephemeral: true});
+
+    const outputChannel = interaction.channel;
+    const dmChannel = await interaction.user.createDM();
+
+    await dmChannel.send({
+        content: 'What do you want to send?'
+    });
+
+    const messageCollector = dmChannel.createMessageCollector({
+        filter: (message) => (message.author.id === interaction.user.id)
+    });
+
+    let proofing = false;
+
+    messageCollector.on('collect', async (message) => {
+        if (proofing) {
+            return;
+        }
+        
+        const responseMessage = await dmChannel.send('React with ✅ to accept or ❌ to make changes.\n\n> ' + message.content);
+
+        await responseMessage.react('✅');
+        await responseMessage.react('❌');
+
+        proofing = true;
+
+        const reactionCollector = responseMessage.createReactionCollector({
+            filter: (reaction, user) => user.id === interaction.user.id
+        });
+
+        reactionCollector.on('collect', async (reaction) => {
+            switch (reaction.emoji.toString()) {
+                case '✅': {
+                    reactionCollector.stop();
+                    messageCollector.stop();
+
+                    await dmChannel.delete();
+                    await outputChannel.send(message.content);
+
+                    return interaction.editReply('Message echoed.');
+                }
+
+                case '❌': {
+                    reactionCollector.stop();
+
+                    await reaction.message.delete();
+                    await dmChannel.send({
+                        content: 'Let\'s try again. What do you want to send?'
+                    });
+
+                    break;
+                }
+
+                default:
+                    await dmChannel.send('Why would you do this? You\'re breakin my heart');
+            }
+
+            proofing = false;
+        });
     });
 };
