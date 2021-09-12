0947786fe2ad786be12eab887406b722f9b7b636
Stephen Hendricks <stephen.hendricks87@gmail.com>
2021-09-12 10:11:52 -0700

    fix db issue for events

--- util/db.js
+++ util/db.js
@@ -8,13 +8,13 @@ async function getDb () {
     }
 
     db = await new Promise((resolve, reject) => {
-        const db = new sqlite3.Database(`./db/${process.env.DB_NAME}.db`, (err) => {
+        const db = new sqlite3.Database(`./db/${process.env.EVENT_DB_NAME}.db`, (err) => {
             if (err) {
                 reject(err);
             }
 
             // eslint-disable-next-line no-console
-            console.log(`Connected to database: ${process.env.DB_NAME}`);
+            console.log(`Connected to database: ${process.env.EVENT_DB_NAME}`);
 
             db.serialize(() => {
                 db.run('CREATE TABLE IF NOT EXISTS events(channel_id text, message_id text, event_ending text)', (err) => {
